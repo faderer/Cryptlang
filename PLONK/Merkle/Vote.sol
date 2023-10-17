3302e02c0861acfbce3532a77e6c08854494ace2
fader <245128111@qq.com>
2023-10-17 22:45:24 +0800

    update ECDSA

--- PLONK/Merkle/Vote.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.8.18;
-contract Vote {
-    mapping (bytes32 => uint) number;
-    function _count (bytes32 _leaf) public 
-    {
-        number[_leaf] += 1;
-    }
-}
--- PLONK/Pedersen/Counting.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.8.18;
-contract Counting {
-    mapping (uint256 => uint) public candidates;
-    function _count (uint256 value) internal 
-    {
-        candidates[value] += 1;
-    }
-}
